bd9f8cb819a439068f7fc97da6246984e6184344
Althea Noelle Sarmiento <113029970+noe-lle@users.noreply.github.com>
2024-10-01 03:26:14 +0000

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\URL;
+$url = config('app.url');
+URL::forceRootUrl($url);
+
 Route::get('/', function () {
     return view('welcome');
 });
