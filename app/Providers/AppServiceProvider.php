9fb70589c764e48a4ecd65ce4b48606e79403188
Althea Noelle Sarmiento <113029970+noe-lle@users.noreply.github.com>
2024-10-01 03:41:31 +0000

    serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
